0f94e676fc24a45902818cf94cff849fe8a086db
Enes <Enes@Enes.mynet>
2022-09-20 23:08:48 +0300

    video 6

---
+++ homework/src/homework/javaVideo_6.java
@@ -0,0 +1,16 @@
+package homework;
+
+public class javaVideo_6 {
+
+	public static void main(String[] args) {
+		int ogrenciSayi = 12;
+		String mesaj = "Ogrenci Sayisi = ";
+		
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println(mesaj + ogrenciSayi);
+		System.out.println("Ogrenci Sayisi = 12");
+		System.out.println("Ogrenci Sayisi = 12");
+	}
+
+}
