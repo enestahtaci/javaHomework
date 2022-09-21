106708ec24544b8fd14a6ec459140fb05a364983
Enes <Enes@Enes.mynet>
2022-09-21 20:58:46 +0300

    recap1

---
+++ homework/src/homework/recapDemo1.java
@@ -0,0 +1,22 @@
+package homework;
+
+public class recapDemo1 {
+
+	public static void main(String[] args) {
+		int sayi1 = 26;
+		int sayi2 = 25;
+		int sayi3 = 2;
+		int enBuyuk = sayi1;
+		
+		if(enBuyuk < sayi2) {
+			enBuyuk = sayi2;
+		}
+		else if (enBuyuk < sayi3) {
+			enBuyuk = sayi3;
+		}
+		
+		System.out.println("En buyuk sayi = "+ enBuyuk);
+		
+	}
+
+}
