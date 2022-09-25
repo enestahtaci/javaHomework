21a8d160e9b6f116af76774a80f2e1e0e1f52ff9
Enes <Enes@Enes.mynet>
2022-09-25 21:09:59 +0300

    new

---
+++ homework/src/homework/loopDemo.java
@@ -0,0 +1,31 @@
+package homework;
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+		//for
+		for(int i=1;i<10;i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("For Döngüsü Bitti");
+		
+		int i=2;
+		//while
+		while(i<10){
+			System.out.println(i);
+			i+=2;
+		}
+		System.out.println("While Döngüsü Bitti");
+		
+		//do while
+		int j=100;
+		do {
+			System.out.println(j);
+			j+=2;
+		}while(j<10);
+			System.out.println("Do While Döngüsü Bitti");
+		
+		
+	}
+
+}
